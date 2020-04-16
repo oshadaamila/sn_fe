53f84d73d44741ce4142af6a4c6e871cc7b0aaba
Oshada <oshadaamila.15@cse.mrt.ac.lk>
2020-04-16 23:04:28 +0530

    adds add-user route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add-user',component: AddUserComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
